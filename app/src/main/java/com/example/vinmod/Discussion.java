74e03e13eebbce7b656142f8eb45065abf712a47
ibrahim5033 <mohamed.siaz@gmail.com>
2020-12-23 03:49:36 -0500

    Main layout fixed

--- app/src/main/java/com/example/vinmod/Discussion.java
+++ app/src/main/java/com/example/vinmod/Discussion.java
@@ -1,19 +1,7 @@
 package com.example.vinmod;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Discussion extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discussion);
-
-
-
-
-
-    }
 }
